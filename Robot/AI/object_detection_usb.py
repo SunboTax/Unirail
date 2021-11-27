f1d2cc32a6c252f2b246fb4e5c90d4f601655698
Wilfried-Sun <53291054+Wilfried-Sun@users.noreply.github.com>
2021-11-27 14:37:03 +0100

    Update object_detection_usb.py

--- Robot/AI/object_detection_usb.py
+++ Robot/AI/object_detection_usb.py
@@ -1,105 +1,90 @@
-"""A demo to classify Raspberry Pi camera stream."""
 import argparse
-import time
-
-import numpy as np
 import os
-import datetime
+import sys
+import time
+import cv2 
+import numpy as np
 
-import edgetpu.detection.engine
-import cv2
-from PIL import Image
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
 
 def main():
-    os.chdir('/home/pi/Unirail/Robot/AI')
-    
+    cam_w, cam_h = 640, 480
+    default_model_dir = '/home/pi/Unirail/Robot/AI'
+    default_model = 'road_signs_quantized.tflite'
+    default_labels = 'road_signs_labels.txt'
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-      '--model', help='File path of Tflite model.', required=False)
-    parser.add_argument(
-      '--label', help='File path of label file.', required=False)
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=5,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--threshold', type=float, default=0.5,
+                        help='classifier score threshold')
     args = parser.parse_args()
-    
-    #args.model = 'test_data/mobilenet_ssd_v2_coco_quant_postprocess.tflite'
-    args.model = 'road_signs_quantized.tflite'
-    args.label = 'road_signs_labels.txt'
-        
-    with open(args.label, 'r') as f:
+
+    with open(args.labels, 'r') as f:
         pairs = (l.strip().split(maxsplit=1) for l in f.readlines())
         labels = dict((int(k), v) for k, v in pairs)
 
-    # initialize open cv
-    IM_WIDTH = 640
-    IM_HEIGHT = 480
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+
+    
     camera = cv2.VideoCapture(0)
-    ret = camera.set(3,IM_WIDTH)
-    ret = camera.set(4,IM_HEIGHT)
+    ret = camera.set(3,cam_w)
+    ret = camera.set(4,cam_h)
     
     font = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (10,IM_HEIGHT-10)
+    bottomLeftCornerOfText = (10,cam_h-10)
     fontScale = 1
     fontColor = (255,255,255)  # white
     boxColor = (0,0,255)   # RED?
     boxLineWidth = 1
     lineType = 2
-    
-    annotate_text = ""
-    annotate_text_time = time.time()
-    time_to_show_prediction = 1.0 # ms
-    min_confidence = 0.20
-    
-    # initial classification engine
-    engine = edgetpu.detection.engine.DetectionEngine(args.model)
-    elapsed_ms = 0
-    
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (IM_WIDTH,IM_HEIGHT))
-    
-    
+
+    inference_size = input_size(interpreter)
+    scale_x, scale_y = cam_w / inference_size[0], cam_h / inference_size[1]
+
     try:
+        last_time = time.monotonic()
         while camera.isOpened():
             try:
-                start_ms = time.time()
-                ret, frame = camera.read() # grab a frame from camera
+                ret, frame = camera.read() 
                 if ret == False :
                     print('can NOT read from camera')
                     break
-
-                input = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # convert to RGB color space
-                img_pil = Image.fromarray(input)
-                #input = cv2.resize(input, (width,height))
-                start_tf_ms = time.time()
-                results = engine.DetectWithImage(img_pil, threshold=min_confidence, keep_aspect_ratio=True,
-                                   relative_coord=False, top_k=5)
-                end_tf_ms = time.time()
-                elapsed_tf_ms = end_tf_ms - start_ms
                 
-                if results :
-                    for obj in results:
-                        
-                        print("%s, %.0f%% %s %.2fms" % (labels[obj.label_id], obj.score *100, obj.bounding_box, elapsed_tf_ms * 1000))
-                        box = obj.bounding_box
-                        coord_top_left = (int(box[0][0]), int(box[0][1]))
-                        coord_bottom_right = (int(box[1][0]), int(box[1][1]))
-                        cv2.rectangle(img, coord_top_left, coord_bottom_right, boxColor, boxLineWidth)
-                        annotate_text = "%s, %.0f%%" % (labels[obj.label_id], obj.score * 100)
-                        coord_top_left = (coord_top_left[0],coord_top_left[1]+15)
-                        cv2.putText(img, annotate_text, coord_top_left, font, fontScale, boxColor, lineType )
-                    print('------')
-                else:
-                    print('No object detected')
+                start_time = time.monotonic()
+                input = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                run_inference(interpreter, np.array(input))
+                results = get_objects(interpreter, args.threshold)[:args.top_k]
+                stop_time = time.monotonic()
+                inference_ms = (stop_time - start_time)*1000.0
+                fps_ms = 1.0 / (stop_time - last_time)
+                last_time = stop_time
 
-                # Print Frame rate info
-                elapsed_ms = time.time() - start_ms
-                annotate_text = "%.2f FPS, %.2fms total, %.2fms in tf " % (1.0 / elapsed_ms, elapsed_ms*1000, elapsed_tf_ms*1000)
-                print('%s: %s' % (datetime.datetime.now(), annotate_text))
-                cv2.putText(img, annotate_text, bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
-                
-                out.write(img)
-                    
-                cv2.imshow('Detected Objects', img)
+                for result in results:
+                    bbox = result.bbox.scale(scale_x, scale_y)
+                    coord_top_left = (bbox.xmin, bbox.ymin)
+                    coord_bottom_right = (bbox.xmin+bbox.width, bbox.ymin+bbox.height)
+                    cv2.rectangle(input, coord_top_left, coord_bottom_right, boxColor, boxLineWidth)
+                    annotate_text = "%s, %.0f%%" % (labels.get(result.id, result.id), result.score * 100)
+                    coord_top_left = (coord_top_left[0],coord_top_left[1]+15)
+                    cv2.putText(input, annotate_text, coord_top_left, font, fontScale, boxColor, lineType )	
+            
+                annotate_text = 'Inference: {:5.2f}ms FPS: {:3.1f}'.format(inference_ms, fps_ms)
+                cv2.imshow('Detected Objects', input)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+
             except:
                 # catch it and don't exit the while loop
                 print('In except')
@@ -108,7 +93,6 @@ def main():
     finally:
         print('In Finally')
         camera.release()
-        out.release()
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
